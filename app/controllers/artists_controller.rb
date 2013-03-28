d1aeaf2e58de6aa8e32798def48512fc53b5844a
Paul <paul.fletcher-hill@yale.edu>
2013-03-28 04:46:25 -0400

    sorting by time

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,7 @@
 class ArtistsController < ApplicationController
   
   def index
-    @artists = Artist.all
+    @artists = Artist.order("time").all
     render json: @artists.to_json(:include => [:images, :tracks])
   end
   
